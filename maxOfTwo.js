e59ab4505ee3f58402956668a2e7676c23eacc84
Derrick Furtado <derrick.furtado10@gmail.com>
2024-01-25 18:59:29 -0700

    repetitive recap of Day 1

---
+++ maxOfTwo.js
@@ -0,0 +1,12 @@
+ x = 4
+ y = 3
+
+
+// {console.log(x)}
+
+if (x === y) {
+    console.log("The numbers are the same")
+} else if (x > y){
+    console.log("X is greater than Y")
+} else 
+    console.log("X is less than Y")
